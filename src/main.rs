fff055002bf9ecaece0f4f1702e67d6138c41eb4
abdullah abdkafi <abdkafi2002@gmail.com>
2022-12-02 14:28:40 +0200

    Day 2: Part 1

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,66 @@
-use std::fs;
+use std::{fs, task::Context};
+
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
+enum Moves {
+    Rock = 1,
+    Paper = 2,
+    Scissors = 3,
+}
+
+impl Moves {
+    fn Elve_Move(input: char) -> Self {
+        match input {
+            'A' => Self::Rock,
+            'B' => Self::Paper,
+            'C' => Self::Scissors,
+            _ => panic!("shouldn't happens"),
+        }
+    }
+    fn My_Move(input: char) -> Self {
+        match input {
+            'X' => Self::Rock,
+            'Y' => Self::Paper,
+            'Z' => Self::Scissors,
+            _ => panic!("shouldn't happens"),
+        }
+    }
+
+    fn Calcluate_Score(Elve: Self, Me: Self) -> u8 {
+        let mut score = 0;
+
+        //add score for move taken
+        score += Me as u8;
+
+        //calculate score for winning
+        score += match (Elve, Me) {
+            (Moves::Rock, Moves::Rock) => 3,
+            (Moves::Rock, Moves::Paper) => 6,
+            (Moves::Rock, Moves::Scissors) => 0,
+            (Moves::Paper, Moves::Rock) => 0,
+            (Moves::Paper, Moves::Paper) => 3,
+            (Moves::Paper, Moves::Scissors) => 6,
+            (Moves::Scissors, Moves::Rock) => 6,
+            (Moves::Scissors, Moves::Paper) => 0,
+            (Moves::Scissors, Moves::Scissors) => 3,
+        };
+
+        score
+    }
+}
 
 fn main() {
     let contents = fs::read_to_string("input.txt").unwrap();
-    
-    let mut index = 0;
-    let mut data: Vec<i64> = Vec::new();
-    data.push(0);
 
-    for i in contents.lines(){
-        if i.is_empty(){
-            index += 1;
-            data.push(0);
-        } else {
-            let num = i64::from_str_radix(i,10).unwrap();
-            data[index] += num;
-        }
-        
+    let mut score = 0;
+
+    for line in contents.lines(){
+        let Elve_move = Moves::Elve_Move(line.chars().nth(0).unwrap());
+
+        let My_move = Moves::My_Move(line.chars().nth(2).unwrap());
+
+        score+= Moves::Calcluate_Score(Elve_move, My_move) as i64; 
+
     }
 
-    
-    let mut data = data.iter().enumerate().collect::<Vec<_>>();
-    data.sort_by(|a,b| a.1.cmp(b.1));
-    data.reverse();
-    
-    println!("{}",data[0..3].iter().fold(0, |acc, &x| acc + x.1));
+    println!("{score}");
 }
