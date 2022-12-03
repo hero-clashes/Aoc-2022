97c49fd6bed9b03a4ce98dccec8f7b943cbde5bf
abdullah abdkafi <abdkafi2002@gmail.com>
2022-12-03 20:57:41 +0200

    Day 3: Part 1

--- src/main.rs
+++ src/main.rs
@@ -1,94 +1,32 @@
-use std::{fs, task::Context};
+use std::{fs, collections::HashSet};
 
-#[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Clone, Copy)]
-enum Moves {
-    Rock = 1,
-    Paper = 2,
-    Scissors = 3,
-}
-
-impl Moves {
-    fn Elve_Move(input: char) -> Self {
-        match input {
-            'A' => Self::Rock,
-            'B' => Self::Paper,
-            'C' => Self::Scissors,
-            _ => panic!("shouldn't happens"),
-        }
-    }
-    fn My_Move(input: char) -> Self {
-        match input {
-            'X' => Self::Rock,
-            'Y' => Self::Paper,
-            'Z' => Self::Scissors,
-            _ => panic!("shouldn't happens"),
-        }
-    }
-
-    fn Calcluate_Score(Elve: Self, Me: Self) -> u8 {
-        let mut score = 0;
-
-        //add score for move taken
-        score += Me as u8;
-
-        //calculate score for winning
-        score += match (Elve, Me) {
-            (Moves::Rock, Moves::Rock) => 3,
-            (Moves::Rock, Moves::Paper) => 6,
-            (Moves::Rock, Moves::Scissors) => 0,
-            (Moves::Paper, Moves::Rock) => 0,
-            (Moves::Paper, Moves::Paper) => 3,
-            (Moves::Paper, Moves::Scissors) => 6,
-            (Moves::Scissors, Moves::Rock) => 6,
-            (Moves::Scissors, Moves::Paper) => 0,
-            (Moves::Scissors, Moves::Scissors) => 3,
-        };
-
-        score
-    }
-
-    fn Get_Move_Needed(Elve_Move: Self, Score_Wanted: char) -> Self {
-        #[derive(PartialEq, Eq, PartialOrd, Ord)]
-        enum Outcome {
-            Lose,
-            Draw,
-            Win,
-        }
-        let outcome = match Score_Wanted {
-            'X' => Outcome::Lose,
-            'Y' => Outcome::Draw,
-            'Z' => Outcome::Win,
-            _ => panic!("shouldn't Hapen"),
-        };
-
-        match outcome {
-            Outcome::Lose => match Elve_Move {
-                Moves::Rock => Moves::Scissors,
-                Moves::Paper => Moves::Rock,
-                Moves::Scissors => Moves::Paper,
-            },
-            Outcome::Draw => Elve_Move,
-            Outcome::Win => match Elve_Move {
-                Moves::Rock => Moves::Paper,
-                Moves::Paper => Moves::Scissors,
-                Moves::Scissors => Moves::Rock,
-            },
-        }
+fn Convert_Char_To_Priorities(ch: char)-> u32{
+    if 'a' <= ch && ch <= 'z'{
+        ch as u32 - 'a' as u32 + 1
+    } else {
+        ch as u32 - 'A' as u32 + 27
     }
 }
 
 fn main() {
     let contents = fs::read_to_string("input.txt").unwrap();
 
-    let mut score = 0;
+    let mut sum = 0;
 
-    for line in contents.lines() {
-        let Elve_move = Moves::Elve_Move(line.chars().nth(0).unwrap());
-
-        let My_move = Moves::Get_Move_Needed(Elve_move, line.chars().nth(2).unwrap());
-
-        score += Moves::Calcluate_Score(Elve_move, My_move) as i64;
+    'outer_loop: for line in contents.lines() {
+        let first_half = &line[0..line.len()/2];
+        let second_half = &line[line.len()/2..line.len()];
+        let mut first_half_set = HashSet::new();
+        for ch in first_half.chars(){
+            first_half_set.insert(ch);
+        }
+        for ch in second_half.chars(){
+            if first_half_set.contains(&ch){
+                sum += Convert_Char_To_Priorities(ch);
+                continue 'outer_loop; 
+            }
+        }
     }
 
-    println!("{score}");
+    println!("{sum}");
 }
