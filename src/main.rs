74bfd5c2ad47ac15107ebd588305027e21e1faf0
abdullah abdkafi <abdkafi2002@gmail.com>
2022-12-04 20:23:32 +0200

    Day 4: Part 1

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,42 @@
-use std::{fs, collections::HashSet};
 use itertools::Itertools;
+use regex::Regex;
+use std::{collections::HashSet, fs, ops::RangeToInclusive};
 
-fn Convert_Char_To_Priorities(ch: char)-> u32{
-    if 'a' <= ch && ch <= 'z'{
-        ch as u32 - 'a' as u32 + 1
-    } else {
-        ch as u32 - 'A' as u32 + 27
+fn range_included(
+    first_range: std::ops::RangeInclusive<i64>,
+    second_range: std::ops::RangeInclusive<i64>,
+) -> bool {
+    for num in first_range {
+        if !second_range.contains(&num) {
+            return false;
+        }
     }
+    return true;
+}
+
+fn range_included_two_ways(
+    first_range: std::ops::RangeInclusive<i64>,
+    second_range: std::ops::RangeInclusive<i64>,
+) -> bool {
+    let first = range_included(first_range.clone(), second_range.clone());
+
+    let second = range_included(second_range, first_range);
+
+    first || second
 }
 
 fn main() {
     let contents = fs::read_to_string("input.txt").unwrap();
 
     let mut sum = 0;
-
-    'outer_loop: for ch in &contents.lines().chunks(3) {
-        let (first_line,second_line,thrid_line) = ch.collect_tuple().unwrap();
-        let first_line_set = HashSet::<_>::from_iter(first_line.to_string().chars());
-        let second_line_set = HashSet::<_>::from_iter(second_line.chars());
-
-        for ch in thrid_line.chars(){
-            if first_line_set.contains(&ch) && second_line_set.contains(&ch){
-                sum += Convert_Char_To_Priorities(ch);
-                continue 'outer_loop; 
-            }
-        }
+    let reg = Regex::new(r"(\d+)-(\d+),(\d+)-(\d+)").unwrap();
+    for line in contents.lines() {
+        let matches = reg.captures(line).unwrap();
+        let first_range = i64::from_str_radix(matches.get(1).unwrap().as_str(), 10).unwrap()
+            ..=i64::from_str_radix(matches.get(2).unwrap().as_str(), 10).unwrap();
+        let second_range = i64::from_str_radix(matches.get(3).unwrap().as_str(), 10).unwrap()
+            ..=i64::from_str_radix(matches.get(4).unwrap().as_str(), 10).unwrap();
+        sum += range_included_two_ways(first_range, second_range) as i64;
     }
 
     println!("{sum}");
