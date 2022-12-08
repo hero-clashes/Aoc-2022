17e576d4cb007beb663109da74dd3c1e04f11c58
abdullah abdkafi <abdkafi2002@gmail.com>
2022-12-08 21:57:34 +0200

    Day 8: Part 2

--- src/main.rs
+++ src/main.rs
@@ -20,71 +20,66 @@ fn main() {
         }
     }
 
-    dbg!(matrix.clone());
 
-    let mut vis_matrix = Array2D::filled_with(false, matrix.num_rows(), matrix.num_rows());
-    for x in 1..vis_matrix.num_columns() - 1 {
-        for y in 1..vis_matrix.num_rows() - 1 {
+    let mut vis_matrix = Array2D::filled_with(0, matrix.num_rows(), matrix.num_rows());
+    for x in 0..vis_matrix.num_columns() {
+        for y in 0..vis_matrix.num_rows() {
             let hight = matrix[(y, x)];
-            let right_check = matrix.row_iter(y).skip(x + 1).fold(true, |mut acc, cur_hight| {
-                if !acc {
+            let right_check = matrix.row_iter(y).skip(x + 1).fold((true,0), |mut acc, cur_hight| {
+                if !acc.0 {
                     return acc;
                 }
                 if *cur_hight >= hight {
-                    acc = false;
+                    acc.0 = false;
                 };
+                acc.1+=1;
                 acc
             });
             let mut left_check = matrix.row_iter(y).collect::<Vec<_>>();
             left_check.resize(x, &0);
             left_check.reverse();
-            let left_check = left_check.iter().fold(true, |mut acc, cur_hight| {
-                if !acc {
+            let left_check = left_check.iter().fold((true,0), |mut acc, cur_hight| {
+                if !acc.0 {
                     return acc;
                 }
                 if **cur_hight >= hight {
-                    acc = false;
+                    acc.0 = false;
                 };
+                acc.1+= 1;
                 acc
             });
-
             let down_check = matrix
                 .column_iter(x)
                 .skip(y + 1)
-                .fold(true, |mut acc, cur_hight| {
-                    if !acc {
+                .fold((true,0), |mut acc, cur_hight| {
+                    if !acc.0 {
                         return acc;
                     }
                     if *cur_hight >= hight {
-                        acc = false;
+                        acc.0 = false;
                     };
+                    acc.1+= 1;
                     acc
                 });
             let mut up_check = matrix.column_iter(x).collect::<Vec<_>>();
             up_check.resize(y, &0);
             up_check.reverse();
-            let up_check = up_check.iter().fold(true, |mut acc, cur_hight| {
-                if !acc {
+            let up_check = up_check.iter().fold((true,0), |mut acc, cur_hight| {
+                if !acc.0 {
                     return acc;
                 }
                 if **cur_hight >= hight {
-                    acc = false;
+                    acc.0 = false;
                 };
+                acc.1+= 1;
                 acc
             });
-            vis_matrix[(y, x)] = right_check || left_check || up_check || down_check;
+            vis_matrix[(y, x)] = right_check.1 * left_check.1 * up_check.1 * down_check.1;
         }
     }
 
-    let outer_layer_visaable = matrix.num_columns() * 2 + (matrix.num_rows() - 2) * 2;
 
-    let score = outer_layer_visaable
-        + vis_matrix.elements_row_major_iter().fold(0, |mut acc, c| {
-            if *c {
-                acc += 1;
-            }
-            acc
-        });
+    let score = vis_matrix.elements_column_major_iter().max().unwrap();
 
     println!("{score}")
 }
