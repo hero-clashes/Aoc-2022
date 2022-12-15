d569fc7d46155b1abbba5e02ab95279ad79f237e
abdullah abdkafi <abdkafi2002@gmail.com>
2022-12-15 23:33:45 +0200

    Day 11: Part 1

--- src/main.rs
+++ src/main.rs
@@ -1,66 +1,113 @@
 use array2d::Array2D;
 use core::num;
+use inpt::{inpt, Inpt};
+use itertools::Itertools;
 use regex::Regex;
 use std::collections::HashSet;
 use std::fs;
 use std::iter::Peekable;
 use std::str::Lines;
 
-fn dest(a: (i32, i32), b: (i32, i32)) -> i32 {
-    (a.0 - b.0).abs() + (a.1 - b.1).abs()
-    // (((a.0 - b.0).pow(2) + (a.1 - b.1).pow(2)) as f32).sqrt() as f32
+#[derive(Inpt, Default, Debug, Clone)]
+#[inpt(regex = r"Operation: new = (.+) (.) (.+)")]
+struct operation {
+    lhs: String,
+    op: String,
+    rhs: String,
+}
+
+impl operation {
+    fn compute(&self, old: i32) -> i32 {
+        let lhs_val = match self.lhs.as_str() {
+            "old" => old,
+            str => str.parse().unwrap(),
+        };
+
+        let rhs_val = match self.rhs.as_str() {
+            "old" => old,
+            str => str.parse().unwrap(),
+        };
+
+        match self.op.as_str() {
+            "*" => lhs_val * rhs_val,
+            "+" => lhs_val + rhs_val,
+            "-" => lhs_val - rhs_val,
+            "/" => lhs_val / rhs_val,
+            _ => todo!()
+        }
+    }
+}
+#[derive(Inpt, Default, Debug, Clone)]
+#[inpt(
+    regex = r"Test: divisible by (\d+)    If true: throw to monkey (\d+)    If false: throw to monkey (\d+)"
+)]
+struct test {
+    divisible_by: i32,
+    true_brench: i32,
+    false_brench: i32,
+}
+impl test {
+    fn item_go_to(&self, worry: i32) -> i32 {
+        if worry % self.divisible_by == 0 {
+            self.true_brench
+        } else {
+            self.false_brench
+        }
+    }
+}
+#[derive(Inpt, Default, Debug, Clone)]
+
+struct Monkey {
+    items: Vec<i32>,
+    oper: operation,
+    test: test,
+    number_of_items_inspected: i32
+}
+
+#[derive(Inpt, Default, Debug)]
+#[inpt(regex = r"Starting items:")]
+struct items {
+    #[inpt(after)]
+    i: Vec<i32>,
 }
 fn main() {
     let contents = fs::read_to_string("input.txt").unwrap();
 
-    let mut output = String::new();
+    let mut Monkeys = Vec::new();
 
-     
-    let mut X = 1;
-    let mut current_cycle = 0;
-    for line in contents.lines() {
-        match line.split_once(" ") {
-            Some(("addx", str)) => {
-              
-                if [X-1, X, X+1].contains(&((current_cycle%40))){
-                    output.push('#');
-                } else {
-                    output.push('.');
-                }
-                current_cycle += 1;
-                if current_cycle%40 == 0{
-                    output.push('\n');
-                }
-                
-                if [X-1, X, X+1].contains(&((current_cycle%40))){
-                    output.push('#');
-                } else {
-                    output.push('.');
-                }
-                current_cycle += 1;
-                if current_cycle%40 == 0{
-                    output.push('\n');
-                }
+    for ch in &contents.lines().chunks(7) {
+        let (f, second, third, fourth, fifth, sixth, s) = ch.collect_tuple().unwrap();
+        let mut monk = Monkey::default();
+        monk.items = inpt::<items>(second).unwrap().i;
+        monk.oper = inpt(third).unwrap();
+        monk.test = inpt(&(fourth.to_string() + &fifth.to_string() + &sixth.to_string())).unwrap();
 
-                X += str.parse::<i32>().unwrap();
+        Monkeys.push(monk);
+    }
+
+    const number_of_rounds: i32 = 20;
+
+    for _ in 0..number_of_rounds {
+        let s = 4 + Monkeys[0].test.false_brench;
+        for i in 0..Monkeys.len() {
+            let current_monkey = Monkeys[i].clone();
+            for item in &current_monkey.items {
+                Monkeys[i].number_of_items_inspected+= 1;
+                let mut worry = *item;
+                worry = current_monkey.oper.compute(worry);
+                worry /= 3;
+
+                Monkeys[current_monkey.test.item_go_to(worry) as usize]
+                    .items
+                    .push(worry);
             }
-            Some((&_, _)) => todo!(),
-            None => {
-                if line == "noop" {
-                    
-                    if [X-1, X, X+1].contains(&((current_cycle%40))){
-                        output.push('#');
-                    } else {
-                        output.push('.');
-                    }
-                    current_cycle += 1;
-                    if current_cycle%40 == 0{
-                        output.push('\n');
-                    }
 
-                }
-            }
+            Monkeys[i].items.clear();
         }
     }
-    println!("{}", output);
+
+    Monkeys.sort_by(|a,b| a.number_of_items_inspected.cmp(&b.number_of_items_inspected));
+    Monkeys.reverse();
+    let score = Monkeys[0].number_of_items_inspected * Monkeys[1].number_of_items_inspected;
+    println!("{}",score);
 }
